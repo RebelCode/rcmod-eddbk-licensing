55bdb7143f8c14bcf176f1eb740f3ab8400ba0aa
Miguel Muscat <miguelmuscat93@gmail.com>
2018-07-04 18:09:53 +0200

    Added EDD license service and config

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 
-return [];
+return [
+    'eddbk_licensing' => [
+        'item_name'   => 'EDD Bookings',
+        'author_name' => 'RebelCode',
+        'server_url'  => 'https://eddbookings.com/edd-sl',
+    ],
+];
--- services.php
+++ services.php
@@ -1,3 +1,16 @@
 <?php
 
-return [];
+use Psr\Container\ContainerInterface;
+
+return [
+    'eddbk_license' => function(ContainerInterface $c) {
+        return new EDD_License(
+            EDDBK_FILE,
+            $c->get('eddbk_licensing/item_name'),
+            EDDBK_VERSION,
+            $c->get('eddbk_licensing/author_name'),
+            null,
+            $c->get('eddbk_licensing/server_url')
+        );
+    },
+];
