fc1a7ae48238774e98c916c46270f6be08b85ec4
Miguel Muscat <miguelmuscat93@gmail.com>
2018-10-03 12:43:20 +0200

    Renamed `item_name` config to `server_name`
    This makes it more clear that this name refers to the item name on the EDD SL server.
    
    EDDBK-125

--- services.php
+++ services.php
@@ -6,7 +6,7 @@ return [
     'eddbk_license' => function(ContainerInterface $c) {
         return new EDD_License(
             $c->get('eddbk_licensing/plugin_file_path'),
-            $c->get('eddbk_licensing/item_name'),
+            $c->get('eddbk_licensing/server_name'),
             $c->get('eddbk_licensing/plugin_version'),
             $c->get('eddbk_licensing/author_name'),
             null,
